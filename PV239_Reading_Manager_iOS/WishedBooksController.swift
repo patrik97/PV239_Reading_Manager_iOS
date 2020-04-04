e5b5b36a2f1dc89b7d6b2704af0b4d5a11517887
Patrik Pluhar <xpluhar@fi.muni.cz>
2020-04-04 16:26:23 +0200

    basic navigation

---
+++ PV239_Reading_Manager_iOS/WishedBooksController.swift
@@ -0,0 +1,13 @@
+//
+//  WishedBooksController.swift
+//  PV239_Reading_Manager_iOS
+//
+//  Created by Patrik Pluhař on 04/04/2020.
+//  Copyright © 2020 FI MU. All rights reserved.
+//
+
+import UIKit
+
+class WishedBooksController: UIViewController {
+    
+}
